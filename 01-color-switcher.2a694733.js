b50426c3684b4d2d4f001ec632e557544213150b
sapphirechristine17 <136240406+sapphirechristine17@users.noreply.github.com>
2023-11-18 23:59:02 +0000

    Deploying to gh-pages from @ sapphirechristine17/goit-js-hw-09@a58127d06f81684065fac54d68f65875c50f1141 🚀

---
+++ 01-color-switcher.2a694733.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");let l=null;t.addEventListener("click",(()=>{l=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(l),l=null,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.2a694733.js.map
